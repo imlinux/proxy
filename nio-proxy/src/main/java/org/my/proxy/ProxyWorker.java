5e6b9a8cde96f8b8959a2c3e3f1d7c0a3857b478
dsy <7518925@gmail.com>
2019-09-27 15:02:17 +0800

    单线程可以和nginx媲美

--- nio-proxy/src/main/java/org/my/proxy/ProxyWorker.java
+++ nio-proxy/src/main/java/org/my/proxy/ProxyWorker.java
@@ -9,7 +9,6 @@ import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -56,8 +55,8 @@ public class ProxyWorker implements Runnable {
 
             if(con == null) break;
 
-            con.getProxySocket().register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE, new ConnWrap(con, ConnType.Proxy));
-            con.getTargetSocket().register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE|SelectionKey.OP_CONNECT, new ConnWrap(con, ConnType.Target));
+            con.getProxySocket().register(selector, SelectionKey.OP_READ, new ConnWrap(con, ConnType.Proxy));
+            con.getTargetSocket().register(selector, SelectionKey.OP_READ, new ConnWrap(con, ConnType.Target));
         }
     }
 
@@ -78,8 +77,6 @@ public class ProxyWorker implements Runnable {
 
                 ConnWrap connWrap = (ConnWrap) selectionKey.attachment();
 
-                SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-
                 if(selectionKey.isValid() && selectionKey.isReadable()) {
 
                     switch (connWrap.connType) {
@@ -92,25 +89,6 @@ public class ProxyWorker implements Runnable {
                             break;
                     }
                 }
-
-
-                if(selectionKey.isValid() && selectionKey.isWritable()) {
-
-                    switch (connWrap.connType) {
-                        case Proxy:
-                            processProxyWrite(connWrap.con);
-                            break;
-
-                        case Target:
-
-                            processTargetWrite(connWrap.con);
-                            break;
-                    }
-                }
-
-                if(selectionKey.isValid() && selectionKey.isConnectable()) {
-                    socketChannel.finishConnect();
-                }
             }
         }
     }
@@ -119,25 +97,28 @@ public class ProxyWorker implements Runnable {
 
         try {
             SocketChannel socketChannel = proxyConnection.getProxySocket();
-
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 1024);
             while (true) {
-                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
                 int n = socketChannel.read(byteBuffer);
 
                 if (n > 0) {
                     byteBuffer.flip();
-                    proxyConnection.getSendQueue().add(byteBuffer);
+                    while (byteBuffer.hasRemaining() && proxyConnection.getTargetSocket().finishConnect()) {
+                        proxyConnection.getTargetSocket().write(byteBuffer);
+                    }
                 }
 
-                if (n <= -1) {
+                if (n == -1) {
                     socketChannel.close();
                     proxyConnection.getTargetSocket().close();
                 }
 
-                if (n < 1024) {
+                if (n == 0) {
                     metrics.meter("proxy-read").mark();
                     break;
                 }
+                byteBuffer.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -148,67 +129,28 @@ public class ProxyWorker implements Runnable {
 
         try {
             SocketChannel socketChannel = proxyConnection.getTargetSocket();
-
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 1024);
             while (true) {
-                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
                 int n = socketChannel.read(byteBuffer);
 
                 if (n > 0) {
                     byteBuffer.flip();
-                    proxyConnection.getRecvQueue().add(byteBuffer);
+                    while (byteBuffer.hasRemaining()) {
+                        proxyConnection.getProxySocket().write(byteBuffer);
+                    }
                 }
 
-                if (n <= -1) {
+                if (n == -1) {
                     socketChannel.close();
+                    proxyConnection.getProxySocket().close();
                 }
 
-                if (n < 1024) {
+                if (n == 0) {
                     metrics.meter("target-recv").mark();
                     break;
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void processProxyWrite(ProxyConnection proxyConnection) {
-
-        try {
-            SocketChannel socketChannel = proxyConnection.getProxySocket();
-
-            BlockingQueue<ByteBuffer> recvQueue = proxyConnection.getRecvQueue();
-
-            while (true) {
-                ByteBuffer byteBuffer = recvQueue.peek();
-
-                if (byteBuffer == null) break;
-
-                socketChannel.write(byteBuffer);
-
-                if (!byteBuffer.hasRemaining()) recvQueue.poll();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void processTargetWrite(ProxyConnection proxyConnection) {
-
-        try {
-            BlockingQueue<ByteBuffer> sendQueue = proxyConnection.getSendQueue();
-
-            SocketChannel socketChannel = proxyConnection.getTargetSocket();
-
-            while (true) {
-                ByteBuffer byteBuffer = sendQueue.peek();
-
-                if (byteBuffer == null) break;
-
-                socketChannel.write(byteBuffer);
-
-                if (!byteBuffer.hasRemaining()) sendQueue.remove();
-
+                byteBuffer.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
